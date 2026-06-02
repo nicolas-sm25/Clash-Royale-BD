136c4e7f60123c8b8f19f4d3f8e4ab4f5ba566ef
Nicolas SM <sorianomedinanicolas@gmail.com>
2026-06-01 19:58:20 -0500

    Implementacion de la ventana principal vacia

--- src/ui/VentanaPrincipal.java
+++ src/ui/VentanaPrincipal.java
@@ -1,4 +1,19 @@
 package ui;
 
-public class VentanaPrincipal {
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+    //Crear ventana principal
+
+    public VentanaPrincipal() {
+
+        setTitle("Base de datos Clash Royale");
+
+        setSize(1000,600);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
 }
